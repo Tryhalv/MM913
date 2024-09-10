5d9a0cec57dec9f68a362cfd84e0ae872a2ae664
Trym Halvorsen <1trymhalvorsen@gmail.com>
2024-09-10 09:22:03 +0200

    ferdig?

---
+++ task1.js
@@ -0,0 +1,15 @@
+//oppgave a 
+let fornavn = "Trym" ;
+//oppgave b 
+let score = 0; 
+//oppgave c
+score += 5;
+//oppgave d 
+score *= 10;
+let telefonNummer = 20192015;
+//oppgave f 
+let fornavnErStudent = true
+//oppgave g 
+console.log ("hei "+fornavn+" din score er " +score);
+//oppgave h
+let highscore = 50;
